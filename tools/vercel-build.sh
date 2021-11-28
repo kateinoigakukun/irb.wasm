52dc34b70f7cd13d9bda412764884d18f64c3141
Yuta Saito <kateinoigakukun@gmail.com>
2021-11-28 16:08:34 +0900

    make a ssh hole

--- tools/vercel-build.sh
+++ tools/vercel-build.sh
@@ -5,8 +5,16 @@ REPO_ROOT="$(cd "$(dirname $0)"/.. && pwd)"
 
 cd "$REPO_ROOT"
 
-amazon-linux-extras install rust1
+curl -LO https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz
+tar -xvzf tmate-2.2.1-static-linux-amd64.tar.gz
+chmod +x ./tmate-2.2.1-static-linux-amd64/tmate
 
-./tools/build-irb.sh
+./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock new-session -d
+./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock wait tmate-ready
+./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 
-parcel build index.html
+# amazon-linux-extras install rust1
+# 
+# ./tools/build-irb.sh
+# 
+# parcel build index.html
