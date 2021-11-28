901e7994ad5d62e6b9b52a1bb487d22c735074ad
Yuta Saito <kateinoigakukun@gmail.com>
2021-11-28 16:21:26 +0900

    restore build script

--- tools/vercel-build.sh
+++ tools/vercel-build.sh
@@ -5,16 +5,10 @@ REPO_ROOT="$(cd "$(dirname $0)"/.. && pwd)"
 
 cd "$REPO_ROOT"
 
-curl -LO https://github.com/tmate-io/tmate/releases/download/2.2.1/tmate-2.2.1-static-linux-amd64.tar.gz
-tar -xvzf tmate-2.2.1-static-linux-amd64.tar.gz
-chmod +x ./tmate-2.2.1-static-linux-amd64/tmate
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y --default-toolchain stable
+source $HOME/.cargo/env
+rustup target add wasm32-unknown-unknown
 
-./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock new-session -d
-./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock wait tmate-ready
-./tmate-2.2.1-static-linux-amd64/tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
+./tools/build-irb.sh
 
-# amazon-linux-extras install rust1
-# 
-# ./tools/build-irb.sh
-# 
-# parcel build index.html
+parcel build index.html
