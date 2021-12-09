e086f38341ddc060dcd6ff31207dae3f3acac95d
Yuta Saito <kateinoigakukun@gmail.com>
2021-12-09 20:20:58 +0900

    update ruby revision

--- tools/build-irb.sh
+++ tools/build-irb.sh
@@ -3,4 +3,4 @@ set -ex
 REPO_ROOT="$(cd "$(dirname $0)"/.. && pwd)"
 OUTPUT_RUBY_WASM_PATH="$REPO_ROOT/static/ruby.wasm"
 
-rbwasm --mapdir /gems::$REPO_ROOT/fake-gems --cruby-src github:kateinoigakukun/ruby@35a2ce1d50189f866eb03341e67a64d448bb9c5c -o $OUTPUT_RUBY_WASM_PATH
+rbwasm --mapdir /gems::$REPO_ROOT/fake-gems --cruby-src github:kateinoigakukun/ruby@9bcc194dc3c12f017a41b6287f85b58f2c487bf8 -o $OUTPUT_RUBY_WASM_PATH
