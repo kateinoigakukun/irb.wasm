cc73aa150c2d5f4aa09d593493a21b34d4b341b5
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-04 21:11:49 +0000

    wip

--- src/irb-worker.ts
+++ src/irb-worker.ts
@@ -1,12 +1,61 @@
 import { WASI } from "@wasmer/wasi";
 import { WasmFs } from "@wasmer/wasmfs";
+import * as path from "path-browserify";
 import { RbValue, RubyVM } from "ruby-head-wasm-wasi"
+
+type IOTask = {
+    kind: string;
+    state: "running" | "done";
+    value1: any | null;
+    value2: RbValue | null;
+}
+
+class ResumptionQueue {
+    private tasks: IOTask[] = [];
+
+    constructor(readonly fiber: RbValue, readonly vm: RubyVM) { }
+
+    schedule(kind: string) {
+        this.tasks.push({ kind, state: "running", value1: null, value2: null });
+    }
+
+    finish(kind: string, value1: any, value2: RbValue | null = null) {
+        let oldestTask: IOTask | null = null;
+        for (let i = 0; i < this.tasks.length; i++) {
+            const task = this.tasks[i];
+            if (task.state === "running" && task.kind === kind) {
+                oldestTask = task;
+                break;
+            }
+        }
+        if (!oldestTask) {
+            throw new Error(`No running task of kind ${kind} found`);
+        }
+        oldestTask.state = "done";
+        oldestTask.value1 = value1;
+        oldestTask.value2 = value2;
+        return this.resumeIfPossible(this.fiber, this.vm);
+    }
+
+    private resumeIfPossible(fiber: RbValue, vm: RubyVM): RbValue | null {
+        let oldestTask = this.tasks.shift();
+        if (oldestTask && oldestTask.state === "done") {
+            if (oldestTask.value2) {
+                return fiber.call("resume", vm.wrap(oldestTask.value1), oldestTask.value2);
+            } else {
+                return fiber.call("resume", vm.wrap(oldestTask.value1));
+            }
+        }
+        return null;
+    }
+}
 
 export class IRB {
     private instance: WebAssembly.Instance | null;
     private wasi: any;
     private vm: RubyVM;
-    irbFiber: RbValue | null;
+    private irbFiber: RbValue | null;
+    private queue: ResumptionQueue | null;
 
     async init(termWriter) {
         const response = await fetch("./irb.wasm");
@@ -37,15 +86,20 @@ export class IRB {
         termWriter("$ # Source code is available at https://github.com/kateinoigakukun/irb.wasm\r\n");
         termWriter("$ " + args.join(" ") + "\r\n");
         const vm = new RubyVM();
+        wasmFs.fs.mkdirSync("/home/me", { mode: 0o777, recursive: true });
         const wasi = new WASI({
             args,
             env: {
                 "GEM_PATH": "/gems",
-                "RUBY_FIBER_MACHINE_STACK_SIZE": String(1024 * 1024 * 10),
+                "RUBY_FIBER_MACHINE_STACK_SIZE": String(1024 * 1024 * 20),
+            },
+            preopens: {
+                "/home/me": "/home/me",
             },
             bindings: {
                 ...WASI.defaultBindings,
                 fs: wasmFs.fs,
+                path: path,
             }
         });
         const imports = {
@@ -68,14 +122,48 @@ export class IRB {
         this.irbFiber = this.vm.eval(`
             require "irb"
             require "stringio"
+            require "js"
 
             ap_path = __FILE__
             STDOUT.sync = true
             $0 = File::basename(ap_path, ".rb") if ap_path
 
-            class JsNonBlockingIO
+            def Dir.home = "/home/me"
+            def Gem.user_home = Dir.home
+
+            class Socket
+                class << self
+                    def method_missing(sym, *) = nil
+                end
+            end
+            class SocketError; end
+
+            require "rubygems/commands/install_command"
+            class Gem::Request
+                def perform_request(request)
+                    puts "gem install #{request}"
+                    response, body_bytes = Fiber.yield(["Gem::Request#perform_request", [request, @uri]])
+                    if body_bytes.is_a?(Array)
+                        body_str = body_bytes.pack("C*")
+                    else
+                        body_str = body_bytes.inspect
+                    end
+                    body_str = Net::BufferedIO.new(StringIO.new(body_str))
+
+                    puts response
+                    status = response["status"].inspect
+                    response_class = Net::HTTPResponse::CODE_TO_OBJ[status]
+                    response = response_class.new("2.0", status.to_i, nil)
+
+                    response.reading_body(body_str, true) {}
+
+                    response
+                end
+            end
+
+            class NonBlockingIO
                 def gets
-                    Fiber.yield.inspect
+                    Fiber.yield(["NonBlockingIO#gets"]).inspect
                 end
 
                 def external_encoding
@@ -95,7 +183,7 @@ export class IRB {
                     alias_method :original_open, :open
                     def fake_open(fd, ...)
                         if fd == 0
-                            return JsNonBlockingIO.new
+                            return NonBlockingIO.new
                         end
                         original_open(fd, ...)
                     end
@@ -110,14 +198,77 @@ export class IRB {
                 irb.run(IRB.conf)
             }
         `)
-        this.irbFiber.call("resume")
+        this.queue = new ResumptionQueue(this.irbFiber, this.vm);
+        const reply = this.irbFiber.call("resume")
+        this.handleIOTask(reply)
+    }
+
+    handleIOTask(task: RbValue) {
+        const kind = task.call("at", this.vm.eval("0")).toJS();
+        this.queue?.schedule(kind);
+        if (kind === "Gem::Request#perform_request") {
+            const args = task.call("at", this.vm.eval("1"));
+            const request = args.call("at", this.vm.eval("0"));
+            const uri = args.call("at", this.vm.eval("1")).toString();
+            const handle = async () => {
+                const response = await fetch(uri, {
+                    method: request.call("method").toString(),
+                    headers: RbToJs.Hash(this.vm, request.call("each").call("to_h")),
+                })
+                let body: RbValue;
+                if (uri.endsWith(".rz")) {
+                    const bodyBuffer = await response.arrayBuffer();
+                    body = JsToRb.Array(this.vm, new Uint8Array(bodyBuffer));
+                } else {
+                    body = this.vm.wrap(await response.text());
+                }
+                const newTask = this.queue?.finish("Gem::Request#perform_request", response, body);
+                if (newTask) {
+                    this.handleIOTask(newTask);
+                }
+            }
+            handle();
+        } else if (kind === "NonBlockingIO#gets") {
+        } else {
+            throw new Error("unknown IO task: " + kind);
+        }
     }
 
     writeLine(line: string) {
-        try {
-            this.irbFiber?.call("resume", this.vm.wrap(line))
-        } catch (e) {
-            console.log(e)
+        const newTask = this.queue?.finish("NonBlockingIO#gets", line);
+        if (newTask) {
+            this.handleIOTask(newTask);
         }
     }
+}
+
+const RbToJs = {
+    Array: (vm: RubyVM, value: RbValue) => {
+        const length = value.call("length").toJS();
+        const items: RbValue[] = [];
+        for (let i = 0; i < length; i++) {
+            const element = value.call("at", vm.eval(String(i)));
+            items.push(element);
+        }
+        return items;
+    },
+    Hash: (vm: RubyVM, value: RbValue) => {
+        const keys = RbToJs.Array(vm, value.call("keys"));
+        const dict = {};
+        for (const key of keys) {
+            const keyString = key.toString();
+            dict[keyString] = value.call("[]", key);
+        }
+        return dict;
+    }
+}
+
+const JsToRb = {
+    Array: (vm: RubyVM, value: Uint8Array): RbValue => {
+        const array = vm.eval("Array.new");
+        for (const item of value) {
+            array.call("push", vm.eval(String(item)));
+        }
+        return array;
+    },
 }
