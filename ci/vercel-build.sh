536a44e0380db9f4878fef597069ad572e9d6c28
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-19 06:25:13 +0000

    install packages via yum

--- ci/vercel-build.sh
+++ ci/vercel-build.sh
@@ -5,8 +5,7 @@ REPO_ROOT="$(cd "$(dirname $0)"/.. && pwd)"
 
 cd "$REPO_ROOT"
 
-apt-get update
-apt-get install ruby bison make autoconf git curl build-essential libyaml-dev zlib1g-dev -y
+yum install ruby bison make autoconf git curl build-essential libyaml-dev zlib1g-dev -y
 
 rake static/irb.wasm
 npx parcel build ./src/index.html
