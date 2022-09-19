675a58982d44ff7b8004016d86c14810c06fbd23
Yuta Saito <kateinoigakukun@gmail.com>
2022-09-19 06:20:50 +0000

    install depdendency packages

--- ci/vercel-build.sh
+++ ci/vercel-build.sh
@@ -5,10 +5,8 @@ REPO_ROOT="$(cd "$(dirname $0)"/.. && pwd)"
 
 cd "$REPO_ROOT"
 
-export WASI_VFS_VERSION=0.1.0
-curl -LO "https://github.com/kateinoigakukun/wasi-vfs/releases/download/v${WASI_VFS_VERSION}/wasi-vfs-cli-x86_64-unknown-linux-gnu.zip"
-unzip wasi-vfs-cli-x86_64-unknown-linux-gnu.zip
-mv wasi-vfs /usr/local/bin/wasi-vfs
+apt-get update
+apt-get install ruby bison make autoconf git curl build-essential libyaml-dev zlib1g-dev -y
 
 rake static/irb.wasm
 npx parcel build ./src/index.html
